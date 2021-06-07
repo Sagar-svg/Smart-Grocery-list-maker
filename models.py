349a3c1dbf351ec782d5623a53ea18d88bf87703
Sagar Thasal <thasalsagar30@gmail.com>
2021-06-07 09:47:37 +0530

    models modified

--- models.py
+++ models.py
@@ -12,7 +12,7 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
-    grocery_list = db.relationship('Grocerylist', backref='owner', lazy=True)
+    grocery_list = db.relationship('Grocerylist', backref='owner', cascade = "all,delete", lazy=True)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
